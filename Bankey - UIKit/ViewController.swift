c51b390bc7f55c5b9cac4dfb44759b7bf62745a9
GeraldyK <gerakdy.gk@gmail.com>
2024-01-31 14:35:23 +0700

    Initial Commit

---
+++ Bankey - UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey - UIKit
+//
+//  Created by Geraldy Kumara on 31/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
